b71f80d0a77de72990797ce8a9b2eba9cd2bd8fc
Nirojan21 <nirojan.yoga@gmail.com>
2022-08-16 21:41:27 +0530

    add server folder structure

---
+++ Server/src/test/java/com/spm/araz/ArazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spm.araz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

09c1bd64de2caf5b0c14bafdacf9bb948ddf4511
Nirojan21 <nirojan.yoga@gmail.com>
2022-08-17 23:27:49 +0530

    add sellers store page

--- Client/src/Pages/Page.js
+++ Client/src/Pages/Page.js
@@ -3,10 +3,12 @@ import { Navigate, Route, Routes } from "react-router";
 //pages
 import CustomerProduct from "./Product_Detail/Product";
 import AdminProduct from "./Product_Manage/Product";
+import Store from "./Store/Store";
 
 function Page() {
   return (
     <Routes>
+      <Route path="/store" element={<Store />} />
       <Route path="/products/view/:id" element={<CustomerProduct />} />
       <Route path="/products/new" element={<AdminProduct />} />
       <Route path="/products/:id" element={<AdminProduct />} />

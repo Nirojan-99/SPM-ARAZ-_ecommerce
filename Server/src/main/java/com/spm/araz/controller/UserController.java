e03fd2c7b7808e0714d25ca5ec97c7c88652feae
Nirojan21 <nirojan.yoga@gmail.com>
2022-09-07 21:39:03 +0530

    bug fixed

--- Server/src/main/java/com/spm/araz/controller/UserController.java
+++ Server/src/main/java/com/spm/araz/controller/UserController.java
@@ -16,173 +16,173 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/User")
 public class UserController {
 
-  @Autowired
-  UserService userService;
+    @Autowired
+    UserService userService;
 
-  @Autowired
-  ProductService productService;
+    @Autowired
+    ProductService productService;
 
-  //add to cart
-  @PostMapping("/cart")
-  public ResponseEntity<UserResponse> addToCart(
-    @RequestParam String productId,
-    @RequestParam int count,
-    @RequestParam String userId
-  ) {
-    Product product = productService.getProduct(productId);
+    //add to cart
+    @PostMapping("/cart")
+    public ResponseEntity<UserResponse> addToCart(
+            @RequestParam String productId,
+            @RequestParam int count,
+            @RequestParam String userId
+    ) {
+        Product product = productService.getProduct(productId);
 
-    User user = userService.getUser(userId);
+        User user = userService.getUser(userId);
 
-    UserResponse userResponse = new UserResponse();
+        UserResponse userResponse = new UserResponse();
 
-    if (product == null || user == null) {
-      userResponse.setMsg("Not found");
-      return new ResponseEntity<>(userResponse, HttpStatus.NOT_FOUND);
-    } else {
-      boolean res = userService.addToCart(user, productId, count);
-      return new ResponseEntity<>(userResponse, HttpStatus.OK);
+        if (product == null || user == null) {
+            userResponse.setMsg("Not found");
+            return new ResponseEntity<>(userResponse, HttpStatus.NOT_FOUND);
+        } else {
+            boolean res = userService.addToCart(user, productId, count);
+            return new ResponseEntity<>(userResponse, HttpStatus.OK);
+        }
     }
-  }
 
-  //remove from cart
-  @PutMapping("/cart")
-  public ResponseEntity<UserResponse> removeFromCart(
-    @RequestParam String userId,
-    @RequestParam String productId
-  ) {
-    Product product = productService.getProduct(productId);
+    //remove from cart
+    @PutMapping("/cart")
+    public ResponseEntity<UserResponse> removeFromCart(
+            @RequestParam String userId,
+            @RequestParam String productId
+    ) {
+        Product product = productService.getProduct(productId);
 
-    User user = userService.getUser(userId);
+        User user = userService.getUser(userId);
 
-    UserResponse userResponse = new UserResponse();
+        UserResponse userResponse = new UserResponse();
 
-    if (product == null || user == null) {
-      userResponse.setMsg("Not found");
-      return new ResponseEntity<>(userResponse, HttpStatus.NOT_FOUND);
-    } else {
-      boolean res = userService.removeItemFromCart(user, productId);
-      return new ResponseEntity<>(userResponse, HttpStatus.OK);
+        if (product == null || user == null) {
+            userResponse.setMsg("Not found");
+            return new ResponseEntity<>(userResponse, HttpStatus.NOT_FOUND);
+        } else {
+            boolean res = userService.removeItemFromCart(user, productId);
+            return new ResponseEntity<>(userResponse, HttpStatus.OK);
+        }
     }
-  }
 
-  //empty cart
-  @DeleteMapping("/cart")
-  public ResponseEntity<UserResponse> emptyCart(@RequestParam String userId) {
-    User user = userService.getUser(userId);
+    //empty cart
+    @DeleteMapping("/cart")
+    public ResponseEntity<UserResponse> emptyCart(@RequestParam String userId) {
+        User user = userService.getUser(userId);
 
-    UserResponse userResponse = new UserResponse();
+        UserResponse userResponse = new UserResponse();
 
-    if (user == null) {
-      userResponse.setMsg("Not found");
-      return new ResponseEntity<>(userResponse, HttpStatus.NOT_FOUND);
-    } else {
-      boolean res = userService.emptyCart(user);
-      return new ResponseEntity<>(userResponse, HttpStatus.OK);
+        if (user == null) {
+            userResponse.setMsg("Not found");
+            return new ResponseEntity<>(userResponse, HttpStatus.NOT_FOUND);
+        } else {
+            boolean res = userService.emptyCart(user);
+            return new ResponseEntity<>(userResponse, HttpStatus.OK);
+        }
     }
-  }
 
-  //get cart
-  @GetMapping("/cart")
-  public ResponseEntity<UserResponse> getCart(@RequestParam String userId) {
-    User user = userService.getUser(userId);
+    //get cart
+    @GetMapping("/cart")
+    public ResponseEntity<UserResponse> getCart(@RequestParam String userId) {
+        User user = userService.getUser(userId);
 
-    UserResponse userResponse = new UserResponse();
+        UserResponse userResponse = new UserResponse();
 
-    if (user == null) {
-      userResponse.setMsg("Not found");
-      return new ResponseEntity<>(userResponse, HttpStatus.NOT_FOUND);
-    } else {
-      userResponse.setUser(user);
-      return new ResponseEntity<>(userResponse, HttpStatus.OK);
+        if (user == null) {
+            userResponse.setMsg("Not found");
+            return new ResponseEntity<>(userResponse, HttpStatus.NOT_FOUND);
+        } else {
+            userResponse.setUser(user);
+            return new ResponseEntity<>(userResponse, HttpStatus.OK);
+        }
     }
-  }
 
-  //add payment
-  @PostMapping("/payment/{id}")
-  public ResponseEntity<UserResponse> addPayment(
-    @RequestBody Payment payment,
-    @PathVariable("id") String id
-  ) {
-    User user = userService.getUser(id);
+    //add payment
+    @PostMapping("/payment/{id}")
+    public ResponseEntity<UserResponse> addPayment(
+            @RequestBody Payment payment,
+            @PathVariable("id") String id
+    ) {
+        User user = userService.getUser(id);
 
-    UserResponse userResponse = new UserResponse();
+        UserResponse userResponse = new UserResponse();
 
-    if (user == null) {
-      userResponse.setMsg("Not found");
-      return new ResponseEntity<>(userResponse, HttpStatus.NOT_FOUND);
-    } else {
-      userService.addPayment(user, payment);
-      return new ResponseEntity<>(userResponse, HttpStatus.OK);
+        if (user == null) {
+            userResponse.setMsg("Not found");
+            return new ResponseEntity<>(userResponse, HttpStatus.NOT_FOUND);
+        } else {
+            userService.addPayment(user, payment);
+            return new ResponseEntity<>(userResponse, HttpStatus.OK);
+        }
     }
-  }
 
-  //remove payment
-  @DeleteMapping("/payment/{id}")
-  public ResponseEntity<UserResponse> deletePayment(
-    @RequestParam int cardNumber,
-    @PathVariable("id") String id
-  ) {
-    User user = userService.getUser(id);
+    //remove payment
+    @DeleteMapping("/payment/{id}")
+    public ResponseEntity<UserResponse> deletePayment(
+            @RequestParam int cardNumber,
+            @PathVariable("id") String id
+    ) {
+        User user = userService.getUser(id);
 
-    UserResponse userResponse = new UserResponse();
+        UserResponse userResponse = new UserResponse();
 
-    if (user == null) {
-      userResponse.setMsg("Not found");
-      return new ResponseEntity<>(userResponse, HttpStatus.NOT_FOUND);
-    } else {
-      userService.deletePayment(user, cardNumber);
-      return new ResponseEntity<>(userResponse, HttpStatus.OK);
+        if (user == null) {
+            userResponse.setMsg("Not found");
+            return new ResponseEntity<>(userResponse, HttpStatus.NOT_FOUND);
+        } else {
+            userService.deletePayment(user, cardNumber);
+            return new ResponseEntity<>(userResponse, HttpStatus.OK);
+        }
     }
-  }
 
-  //new user
-  @PostMapping("")
-  public ResponseEntity<UserResponse> addUser(
-    @RequestBody(required = true) User user
-  ) {
-    User res = userService.createUser(user);
+    //new user
+    @PostMapping("")
+    public ResponseEntity<UserResponse> addUser(
+            @RequestBody(required = true) User user
+    ) {
+        User res = userService.createUser(user);
 
-    UserResponse userResponse = new UserResponse();
-    if (res.getId() != null) {
-      User user1 = new User();
-      user1.setId(res.getId());
-      user1.setUserType(res.getUserType());
-      userResponse.setUser(user1);
-      userResponse.setMsg("User created");
-      return new ResponseEntity<>(userResponse, HttpStatus.OK);
-    } else {
-      userResponse.setMsg("Unable to create User");
-      return new ResponseEntity<>(userResponse, HttpStatus.BAD_REQUEST);
+        UserResponse userResponse = new UserResponse();
+        if (res.getId() != null) {
+            User user1 = new User();
+            user1.setId(res.getId());
+            user1.setUserType(res.getUserType());
+            userResponse.setUser(user1);
+            userResponse.setMsg("User created");
+            return new ResponseEntity<>(userResponse, HttpStatus.OK);
+        } else {
+            userResponse.setMsg("Unable to create User");
+            return new ResponseEntity<>(userResponse, HttpStatus.BAD_REQUEST);
+        }
     }
-  }
 
-  //get user by email for login
-  @PostMapping("/login")
-  public ResponseEntity<UserResponse> getUserByEmail(
-    @RequestParam("email") String email,
-    @RequestParam("password") String password
-  ) {
-    User user = userService.getByEmail(email);
+    //get user by email for login
+    @PostMapping("/login")
+    public ResponseEntity<UserResponse> getUserByEmail(
+            @RequestParam("email") String email,
+            @RequestParam("password") String password
+    ) {
+        User user = userService.getByEmail(email);
 
-    UserResponse userResponse = new UserResponse();
+        UserResponse userResponse = new UserResponse();
 
-    if (user == null) {
-      userResponse.setMsg("Email Not found");
+        if (user == null) {
+            userResponse.setMsg("Email Not found");
 
-      return new ResponseEntity<>(userResponse, HttpStatus.NOT_FOUND);
-    } else {
-      if (user.getPassword().equals(password)) {
-        User user1 = new User();
-        user1.setEmail(user.getEmail());
-        user1.setId(user.getId());
-        user1.setUserType(user.getUserType());
-        userResponse.setUser(user1);
-        userResponse.setMsg("Valid Email and Password");
-      } else {
-        userResponse.setMsg("Invalid Email and Password");
-      }
+            return new ResponseEntity<>(userResponse, HttpStatus.NOT_FOUND);
+        } else {
+            if (user.getPassword().equals(password)) {
+                User user1 = new User();
+                user1.setEmail(user.getEmail());
+                user1.setId(user.getId());
+                user1.setUserType(user.getUserType());
+                userResponse.setUser(user1);
+                userResponse.setMsg("Valid Email and Password");
+            } else {
+                userResponse.setMsg("Invalid Email and Password");
+            }
 
-      return new ResponseEntity<>(userResponse, HttpStatus.OK);
+            return new ResponseEntity<>(userResponse, HttpStatus.OK);
+        }
     }
-  }
 }

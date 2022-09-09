35600b7bfd9a2d3ef7ee67ec6968e23ca25629dd
Arivu <thillaiarivu2000@gmail.com>
2022-09-09 15:25:38 +0530

    Add some validation for signup

--- Client/src/Pages/Signup/Signup.js
+++ Client/src/Pages/Signup/Signup.js
@@ -10,11 +10,101 @@ import { Container } from "@mui/system";
 import ButtonA from "../../Components/ButtonA";
 import Input from "../../Components/Input";
 import Label from "../../Components/Label";
+import { login } from "../../Store/auth";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
 
 function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cpassword, setCPassword] = useState("");
+  const [contactNo, setContactNo] = useState("");
+  const [address, setAddress] = useState("");
+  const [gender, setGender] = useState();
+  const [dob, setDob] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [error, setError] = useState(false);
+
+  const OnSubmitHandler = () => {
+    setError(false);
+    //validation
+    if (!name.trim() || name.length < 2) {
+      toast("Enter valid Name", { type: "error" });
+      return setError(true);
+    }
+    if (!email.trim() || !email.includes("@") || !email.includes(".")) {
+      toast("Enter valid Email", { type: "error" });
+      return setError(true);
+    }
+    if (!password.trim() || password.length < 6) {
+      toast("Enter valid Password", { type: "error" });
+      return setError(true);
+    }
+    if (!cpassword.trim() || cpassword.length < 6 || !(cpassword == password)) {
+      toast("Please check conform password", { type: "error" });
+      return setError(true);
+    }
+    if (!contactNo.trim() || contactNo.length < 8) {
+      toast("Enter valid Contact Number", { type: "error" });
+      return setError(true);
+    }
+    if (!address.trim() || address.length < 3) {
+      toast("Enter valid Address", { type: "error" });
+      return setError(true);
+    }
+    if (!gender == "") {
+      toast("Select a Gender", { type: "error" });
+      return setError(true);
+    }
+    if (!dob.trim()) {
+      toast("Enter valid Date of birth", { type: "error" });
+      return setError(true);
+    }
+    const data = {
+      name: name,
+      email: email,
+      password: password,
+      userType: "buyer",
+      contactNo: contactNo,
+      address: address,
+      gender: gender,
+      dob: dob,
+    };
+    axios
+      .post(`http://localhost:5000/User`, data)
+      .then((res) => {
+        dispatch(
+          login({
+            role: res.data.user.userType,
+            userID: res.data.user.id,
+            /*token: res.data.token*/
+          })
+        );
+        setTimeout(() => {
+          toast("Registered Sucess", { type: "success" });
+        }, 1000);
+
+        setTimeout(() => {
+          navigate("/profile/details");
+        }, 1500);
+      })
+
+      .catch(() => {
+        toast("Invalid Email and Password", { type: "error" });
+      });
+  };
+
   return (
     <>
       <Box>
+        <ToastContainer />
         <Container maxWidth="sm">
           {/* title */}
 
@@ -33,9 +123,16 @@ function SignUp() {
             >
               Register
             </Typography>
-            {/* store name */}
-            <Label title="UserName" for="store_name" />
-            <Input id="uname" autoFocus={true} size="small" type="text" />
+            {/* user name */}
+            <Label title="UserName" for="uname" />
+            <Input
+              id="uname"
+              autoFocus={true}
+              size="small"
+              type="text"
+              value={name}
+              set={setName}
+            />
             {/* Email */}
             <Label title="Email" for="email" />
             <Input
@@ -43,6 +140,8 @@ function SignUp() {
               size="small"
               placeholder="xxxxxx@gmail.com"
               type="text"
+              value={email}
+              set={setEmail}
             />
             {/* contact number */}
             <Label title="Contact Number" for="contact_number" />
@@ -51,6 +150,8 @@ function SignUp() {
               size="small"
               placeholder="07xxxxxxxx"
               type="number"
+              value={contactNo}
+              set={setContactNo}
             />
             {/* address */}
             <Label for="address" title="Address" />
@@ -61,16 +162,56 @@ function SignUp() {
               maxRows={4}
               type="text"
               size="small"
+              value={address}
+              set={setAddress}
             />
+            {/* gender */}
+            <Label title="Gender" for="gender" />
+            <Select
+              sx={{ mb: 1, color: "#1597BB", fontWeight: "500" }}
+              fullWidth
+              required
+              size="small"
+              color="info"
+              id="gender"
+              value={gender}
+              set={setGender}
+            >
+              {/* <option value="male">Male</option>
+              <option value="female">Female</option> */}
+              <MenuItem value={"male"} selected>
+                Male
+              </MenuItem>
+              <MenuItem value={"female"}>Female</MenuItem>
+            </Select>
+            {/* dob */}
+            <Label title="Date of Birth" for="dob" />
+            <Input id="dob" size="small" type="date" value={dob} set={setDob} />
             {/* Password */}
             <Label for="password" title="Password" />
-            <Input id="password" type="password" size="small" />
+            <Input
+              id="password"
+              type="password"
+              size="small"
+              value={password}
+              set={setPassword}
+            />
             {/* Confirm Password */}
             <Label for="re_password" title="Confirm Password" />
-            <Input id="re_password" type="password" size="small" />
+            <Input
+              id="re_password"
+              type="password"
+              size="small"
+              value={cpassword}
+              set={setCPassword}
+            />
             {/* save button */}
             <Box mt={2} />
-            <ButtonA fullWidth={true} title="REGISTER" />
+            <ButtonA
+              fullWidth={true}
+              title="REGISTER"
+              handler={OnSubmitHandler}
+            />
             <Box mt={2} />
           </Box>
         </Container>

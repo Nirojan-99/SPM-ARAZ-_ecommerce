c63b5621f88df4d11aeed2afc644696dd1bcd413
Nirojan21 <nirojan.yoga@gmail.com>
2022-09-19 21:59:27 +0530

    add about-us page

--- Client/src/Pages/AboutUs/AboutUs.js
+++ Client/src/Pages/AboutUs/AboutUs.js
@@ -1,7 +1,79 @@
-import React from 'react'
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import { Container } from "@mui/system";
 
 export default function AboutUs() {
   return (
-    <div>AboutUs</div>
-  )
+    <>
+      <Box sx={{ position: "relative" }}>
+        <img
+          style={{ width: "100%", height: "400px", resize: "cover" }}
+          src={
+            "https://fscl01.fonpit.de/userfiles/7715851/image/NextPit-Developer-Mode-w1400h788.jpg"
+          }
+        />
+        <Box
+          sx={{ position: "absolute", top: 250, left: 0, right: 0, flex: 1 }}
+        >
+          <Typography
+            sx={{
+              color: "#fff",
+              fontSize: { sm: 40, xs: 20 },
+              textAlign: "center",
+            }}
+          >
+            Delivering happiness on the go
+          </Typography>
+          <Typography
+            sx={{
+              color: "#fff",
+              fontSize: { sm: 30, xs: 15 },
+              textAlign: "center",
+            }}
+          >
+            Happy Shopping
+          </Typography>
+        </Box>
+        {/* body */}
+        <Container maxWidth="lg" m={0} p={0}>
+          <Box sx={{ flexDirection: "row", justifyContent: "center", flex: 1 }}>
+            <img
+              style={{ width: "100%" }}
+              src="https://laz-img-cdn.alicdn.com/images/ims-web/TB1K3i9dBLoK1RjSZFuXXXn0XXa.jpg_2200x2200q75.jpg_.webp"
+            />
+          </Box>
+          <Box sx={{ flexDirection: "row", justifyContent: "center", flex: 1 }}>
+            <img
+              style={{ width: "100%" }}
+              src="https://laz-img-cdn.alicdn.com/images/ims-web/TB18SO.dCzqK1RjSZFpXXakSXXa.jpg_2200x2200q75.jpg_.webp"
+            />
+          </Box>
+          <Box sx={{ flexDirection: "row", justifyContent: "center", flex: 1 }}>
+            <img
+              style={{ width: "100%" }}
+              src="https://laz-img-cdn.alicdn.com/images/ims-web/TB1AEL.dOrpK1RjSZFhXXXSdXXa.jpg_2200x2200q75.jpg_.webp"
+            />
+          </Box>
+          <Box sx={{ flexDirection: "row", justifyContent: "center", flex: 1 }}>
+            <img
+              style={{ width: "100%" }}
+              src="https://laz-img-cdn.alicdn.com/images/ims-web/TB1T5YBdyLaK1RjSZFxXXamPFXa.jpg_2200x2200q75.jpg_.webp"
+            />
+          </Box>
+          <Box sx={{ flexDirection: "row", justifyContent: "center", flex: 1 }}>
+            <img
+              style={{ width: "100%" }}
+              src="https://laz-img-cdn.alicdn.com/images/ims-web/TB1T5YBdyLaK1RjSZFxXXamPFXa.jpg_2200x2200q75.jpg_.webp"
+            />
+          </Box>
+          <Box sx={{ flexDirection: "row", justifyContent: "center", flex: 1 }}>
+            <img
+              style={{ width: "100%" }}
+              src="https://laz-img-cdn.alicdn.com/images/ims-web/TB17wbidyrpK1RjSZFhXXXSdXXa.jpg_2200x2200q75.jpg_.webp"
+            />
+          </Box>
+        </Container>
+      </Box>
+    </>
+  );
 }
